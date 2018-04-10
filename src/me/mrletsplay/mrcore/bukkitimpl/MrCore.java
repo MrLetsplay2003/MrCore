6d1a5aeb2ad4e80b221b3f05cfa3990e05ece119
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-04-10 22:02:00 +0200

    Added bukkitimpl

---
+++ src/me/mrletsplay/mrcore/bukkitimpl/MrCore.java
@@ -0,0 +1,12 @@
+package me.mrletsplay.mrcore.bukkitimpl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MrCore extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		getLogger().info("And MrCore is on board as well! :wave:");
+	}
+	
+}
