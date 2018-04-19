ac272eed793334442003bb493aa8c323692bd3fd
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-04-19 16:40:02 +0200

    Renamed some methods

--- src/me/mrletsplay/mrcore/main/MrCore.java
+++ src/me/mrletsplay/mrcore/main/MrCore.java
@@ -2,6 +2,11 @@ package me.mrletsplay.mrcore.main;
 
 public class MrCore {
 
+	/*
+	 * TODO:
+	 * - Color chat message parser (for BaseComponents)
+	 */
+	
 	public static final String VERSION = "v1.4";
 	
 }
