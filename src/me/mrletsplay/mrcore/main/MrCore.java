fdbf3336cbfa5a6bbf4938768cbf3e7f4564f8e0
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-04-21 12:02:39 +0200

    Added OtherTools.similarity

--- src/me/mrletsplay/mrcore/main/MrCore.java
+++ src/me/mrletsplay/mrcore/main/MrCore.java
@@ -6,6 +6,8 @@ public class MrCore {
 	 * TODO:
 	 * - Color chat message parser (for BaseComponents)
 	 * - Load/save items (CC)
+	 * - NMS
+	 * - CC custom parser
 	 */
 	
 	public static final String VERSION = "v1.4";
