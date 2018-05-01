9573df57a48e88a43536feac4f8a44cfbf1cdb66
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-05-01 12:31:42 +0200

    Removed some debugging stuff

--- src/me/mrletsplay/mrcore/bukkitimpl/MrCorePlugin.java
+++ src/me/mrletsplay/mrcore/bukkitimpl/MrCorePlugin.java
@@ -1,23 +1,11 @@
 package me.mrletsplay.mrcore.bukkitimpl;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.mrletsplay.mrcore.bukkitimpl.ChatUI.ItemSupplier;
-import me.mrletsplay.mrcore.bukkitimpl.ChatUI.StaticUIElement;
-import me.mrletsplay.mrcore.bukkitimpl.ChatUI.UIBuilderMultiPage;
-import me.mrletsplay.mrcore.bukkitimpl.ChatUI.UIElement;
-import me.mrletsplay.mrcore.bukkitimpl.ChatUI.UIElementAction;
-import me.mrletsplay.mrcore.bukkitimpl.ChatUI.UIElementActionEvent;
-import me.mrletsplay.mrcore.bukkitimpl.ChatUI.UILayoutMultiPage;
 import me.mrletsplay.mrcore.bukkitimpl.ChatUI.UIListener;
-import me.mrletsplay.mrcore.bukkitimpl.ChatUI.UIMultiPage;
 import me.mrletsplay.mrcore.bukkitimpl.GUIUtils.GUIListener;
 
 public class MrCorePlugin extends JavaPlugin{
