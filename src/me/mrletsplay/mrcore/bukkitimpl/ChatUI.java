a71913c01a240283f7def7b8b9fbc8f7d33d70d1
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-05-03 09:27:34 +0200

    Updated JavaDocs

--- src/me/mrletsplay/mrcore/bukkitimpl/ChatUI.java
+++ src/me/mrletsplay/mrcore/bukkitimpl/ChatUI.java
@@ -25,27 +25,52 @@ public class ChatUI {
 		public UILayout layout;
 		private HashMap<String, Object> properties;
 		
+		/**
+		 * Constructs a UIBuilder
+		 * @see <a href="https://github.com/MrLetsplay2003/MrCore/wiki/ChatUI">ChatUI wiki</a>
+		 */
 		public UIBuilder() {
 			elements = new HashMap<>();
 			properties = new HashMap<>();
 			layout = new UILayout();
 		}
 
+		/**
+		 * Adds an element to the UI
+		 * @param key The key of the element. This should be a unique value
+		 * @param element The {@link UIElement} to add under that key
+		 * @return This UIBuilder instance
+		 */
 		public UIBuilder addElement(String key, UIElement element) {
 			elements.put(key, element);
 			return this;
 		}
 		
+		/**
+		 * Sets the UILayout for the UI
+		 * @param layout The {@link UILayout} to use
+		 * @return This UIBuilder instance
+		 */
 		public UIBuilder setLayout(UILayout layout) {
 			this.layout = layout;
 			return this;
 		}
 		
+		/**
+		 * Sets the default properties for this UI<br>
+		 * For more info, please see the <a href="https://github.com/MrLetsplay2003/MrCore/wiki/ChatUI">ChatUI wiki</a>
+		 * @param properties The property map to use
+		 * @return This UIBuilder instance
+		 */
 		public UIBuilder setProperties(HashMap<String, Object> properties) {
 			this.properties = properties;
 			return this;
 		}
 		
+		/**
+		 * Builds this UIBuilder into a {@link UI} instance
+		 * @return
+		 */
 		public UI build() {
 			return new UI(this);
 		}
@@ -56,6 +81,10 @@ public class ChatUI {
 		
 		private ItemSupplier<T> supplier;
 		
+		/**
+		 * Constructs a UIBuilderMultiPage instance
+		 * @see <a href="https://github.com/MrLetsplay2003/MrCore/wiki/ChatUI">ChatUI wiki</a>
+		 */
 		public UIBuilderMultiPage() {
 			super();
 		}
