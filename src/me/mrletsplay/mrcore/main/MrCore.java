06c73f99c4baf24a1c1f04926f953402f86d9530
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-05-10 19:20:56 +0200

    Changed version to 1.7

--- src/me/mrletsplay/mrcore/main/MrCore.java
+++ src/me/mrletsplay/mrcore/main/MrCore.java
@@ -14,7 +14,7 @@ public class MrCore {
 	 * - MySQL
 	 */
 	
-	private static final String VERSION = "1.6";
+	private static final String VERSION = "1.7";
 	
 	public static String getVersion() {
 		return VERSION;
