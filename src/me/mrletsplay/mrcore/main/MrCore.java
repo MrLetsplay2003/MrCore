6682631c49afb01dc60cd82df337c1ab985165d1
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-05-19 14:30:08 +0200

    Changed version to 1.8.1

--- src/me/mrletsplay/mrcore/main/MrCore.java
+++ src/me/mrletsplay/mrcore/main/MrCore.java
@@ -15,7 +15,7 @@ public class MrCore {
 	 * - Multi-Plugin ("Modules")
 	 */
 	
-	private static final String VERSION = "1.8";
+	private static final String VERSION = "1.8.1";
 	
 	public static String getVersion() {
 		return VERSION;
