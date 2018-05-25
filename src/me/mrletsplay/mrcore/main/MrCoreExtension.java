26650368e36689dab79db741fca974eae2310a06
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-05-25 15:12:23 +0200

    experimental

--- src/me/mrletsplay/mrcore/main/MrCoreExtension.java
+++ src/me/mrletsplay/mrcore/main/MrCoreExtension.java
@@ -1,8 +1,9 @@
 package me.mrletsplay.mrcore.main;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -12,19 +13,24 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.InvalidDescriptionException;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginBase;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class MrCoreExtension implements Plugin {
 
+	private JARLoader loader;
+	
 	public MrCoreExtension() {
 		Bukkit.getPluginManager().enablePlugin(this);
 		if(!(getClassLoader() instanceof JARLoader)) {
 			throw new RuntimeException("yay: "+getClassLoader().getClass().getName());
 		}
+	}
+	
+	public void init(JARLoader loader) {
+		this.loader = loader;
 	}
 	
 	public ClassLoader getClassLoader() {
@@ -33,127 +39,112 @@ public class MrCoreExtension implements Plugin {
 
 	@Override
 	public List<String> onTabComplete(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public FileConfiguration getConfig() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public File getDataFolder() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ChunkGenerator getDefaultWorldGenerator(String arg0, String arg1) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public PluginDescriptionFile getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			System.out.println(loader.getResource("plugin.yml"));
+			return new PluginDescriptionFile(new InputStreamReader(loader.getResource("plugin.yml").openStream()));
+		} catch (InvalidDescriptionException | IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
 	public Logger getLogger() {
-		// TODO Auto-generated method stub
-		return null;
+		return Logger.getLogger(getName());
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "MrCore_extension";
 	}
 
 	@Override
 	public PluginLoader getPluginLoader() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public InputStream getResource(String arg0) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Server getServer() {
-		// TODO Auto-generated method stub
-		return null;
+		return Bukkit.getServer();
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isNaggable() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void onDisable() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onEnable() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onLoad() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void reloadConfig() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void saveConfig() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void saveDefaultConfig() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void saveResource(String arg0, boolean arg1) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void setNaggable(boolean arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 	
