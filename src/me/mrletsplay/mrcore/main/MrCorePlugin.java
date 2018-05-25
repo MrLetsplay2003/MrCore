a7c4df938fdbb2b48b9f113c4074fd183e4c4d1b
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-05-25 13:46:52 +0200

    experimental

---
+++ src/me/mrletsplay/mrcore/main/MrCorePlugin.java
@@ -0,0 +1,14 @@
+package me.mrletsplay.mrcore.main;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MrCorePlugin extends JavaPlugin {
+
+	@Deprecated
+	@Override
+	public FileConfiguration getConfig() {
+		return super.getConfig();
+	}
+	
+}
