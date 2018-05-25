e5c185e6f200c0bffc4e98c1a045513acc2b0f01
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-05-25 13:48:30 +0200

    experimental

--- src/me/mrletsplay/mrcore/main/MrCoreExtension.java
+++ src/me/mrletsplay/mrcore/main/MrCoreExtension.java
@@ -3,7 +3,7 @@ package me.mrletsplay.mrcore.main;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MrCorePlugin extends JavaPlugin {
+public class MrCoreExtension extends JavaPlugin {
 
 	@Deprecated
 	@Override
