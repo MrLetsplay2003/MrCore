df1508d1e2cb4232484b84b6cbc348f9e1bad557
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-06-10 18:28:58 +0200

    Changed some stuff

--- src/me/mrletsplay/mrcore/bukkitimpl/GUIUtils.java
+++ src/me/mrletsplay/mrcore/bukkitimpl/GUIUtils.java
@@ -304,7 +304,7 @@ public class GUIUtils {
 		
 		public abstract List<T> getItems();
 		
-		public abstract GUIElement toGUIElement(Player p, T item);
+		public abstract GUIElement toGUIElement(GUIBuildEvent event, T item);
 		
 	}
 	
@@ -835,13 +835,15 @@ public class GUIUtils {
 			holder.getProperties().put("page", page);
 			List<Integer> slots = builder.customItemSlots;
 			int nSlots = slots.size();
-			ItemSupplier<T> supp = builder.supplier;
-			List<T> items = supp.getItems();
-			List<GUIElement> elements = items.stream().map(i -> supp.toGUIElement(p, i)).collect(Collectors.toList());
-			Map<Integer, GUIElement> elSlots = new HashMap<>();
-			int pages = items.size()/nSlots;
 			
 			GUIBuildEvent event = new GUIBuildEvent(holder, p, base);
+			
+			ItemSupplier<T> supp = builder.supplier;
+			List<T> items = supp.getItems();
+			
+			List<GUIElement> elements = items.stream().map(i -> supp.toGUIElement(event, i)).collect(Collectors.toList());
+			Map<Integer, GUIElement> elSlots = new HashMap<>();
+			int pages = items.size()/nSlots;
 			
 			if(page <= pages && page >= 0) {
 				int start = page*nSlots;
