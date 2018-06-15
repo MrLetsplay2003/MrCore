efafbe6cc299ba9c33157c8ab93d355466332afa
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-06-15 18:13:40 +0200

    Added Bukkit api & expanded SpiGet api

---
+++ src/me/mrletsplay/mrcore/bukkitapi/BukkitFile.java
@@ -0,0 +1,53 @@
+package me.mrletsplay.mrcore.bukkitapi;
+
+import me.mrletsplay.mrcore.misc.JSON.JSONObject;
+
+public class BukkitFile {
+
+	private int projectId;
+	private String downloadURL, fileName, fileURL, gameVersion, md5, name, releaseType;
+	
+	public BukkitFile(JSONObject bukkitResponse) {
+		this.projectId = bukkitResponse.getInt("projectId");
+		this.downloadURL = bukkitResponse.getString("downloadUrl");
+		this.fileName = bukkitResponse.getString("fileName");
+		this.fileURL = bukkitResponse.getString("fileUrl");
+		this.gameVersion = bukkitResponse.getString("gameVersion");
+		this.md5 = bukkitResponse.getString("md5");
+		this.name = bukkitResponse.getString("name");
+		this.releaseType = bukkitResponse.getString("releaseType");
+	}
+	
+	public int getProjectID() {
+		return projectId;
+	}
+	
+	public String getDownloadURL() {
+		return downloadURL;
+	}
+	
+	public String getFileName() {
+		return fileName;
+	}
+	
+	public String getFileURL() {
+		return fileURL;
+	}
+	
+	public String getGameVersion() {
+		return gameVersion;
+	}
+	
+	public String getMD5() {
+		return md5;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getReleaseType() {
+		return releaseType;
+	}
+	
+}
