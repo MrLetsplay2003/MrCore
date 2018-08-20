ebfe27300eadac5aa64d067ebe761ab258847440
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-08-20 21:43:56 +0200

    Changed version to 1.10

--- src/me/mrletsplay/mrcore/main/MrCore.java
+++ src/me/mrletsplay/mrcore/main/MrCore.java
@@ -2,7 +2,7 @@
 
 public class MrCore {
 	
-	private static final String VERSION = "1.9.10";
+	private static final String VERSION = "1.10";
 	
 	public static String getVersion() {
 		return VERSION;
