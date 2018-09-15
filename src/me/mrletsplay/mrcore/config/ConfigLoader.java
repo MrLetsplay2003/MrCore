d8a6f98d254f806321599012f46b34aca4470c70
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-09-15 11:56:14 +0200

    Added ConfigLoader.loadConfig(InputStream, ConfigSaveProperty...)

--- src/me/mrletsplay/mrcore/config/ConfigLoader.java
+++ src/me/mrletsplay/mrcore/config/ConfigLoader.java
@@ -1,7 +1,10 @@
 package me.mrletsplay.mrcore.config;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.stream.Collectors;
 
@@ -39,6 +42,42 @@ public class ConfigLoader {
 			} catch (IOException e1) {
 				throw new InvalidConfigException("Failed to load config from file", -1);
 			}
+		}
+	}
+
+	/**
+	 * Loads the given InputStream as a CustomConfig<br>
+	 * The returned CustomConfig instance will be as loaded from a file with a file parameter of null<br>
+	 * This method will apply conversion using the {@link ConfigConverter#convertToLatestVersion(String, CustomConfig, ConfigVersion)} method if needed
+	 * @param in The InputStream to load
+	 * @param defaultSaveProperties The default save properties (See {@link CustomConfig#CustomConfig(File, ConfigSaveProperty...)})
+	 * @throws InvalidConfigException If an IO error occurs while loading/converting the config
+	 * @return A loaded CustomConfig instance
+	 */
+	public static CustomConfig loadConfig(InputStream in, ConfigSaveProperty... defaultSaveProperties) {
+		CustomConfig cc = new CustomConfig((File) null, defaultSaveProperties);
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			byte[] b = new byte[4096];
+			int len;
+			while((len = in.read(b)) > 0) {
+				out.write(b, 0, len);
+			}
+			try {
+				cc.loadConfig(in);
+				return cc;
+			}catch(InvalidConfigVersionException e) {
+				ConfigVersion v = ConfigVersion.getByName(e.getVersion());
+				if(v == null) throw new InvalidConfigVersionException("Invalid version: "+e.getVersion(), null);
+				CustomConfig cfg = ConfigConverter.convertToLatestVersion(
+						new String(out.toByteArray(), StandardCharsets.UTF_8),
+						cc,
+						v);
+				cfg.saveConfigSafely();
+				return cfg;
+			}
+		}catch(IOException e2) {
+			throw new InvalidConfigException("Failed to load config from InputStream", -1);
 		}
 	}
 
