89d7d1d466c92b65bd00ab596e2565fb877f1c5d
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-09-18 19:35:15 +0200

    Updated versioned utils

--- src/me/mrletsplay/mrcore/bukkitimpl/versioned/NMSVersion.java
+++ src/me/mrletsplay/mrcore/bukkitimpl/versioned/NMSVersion.java
@@ -14,9 +14,10 @@ public enum NMSVersion {
 	V1_10_R1("MC 1.10", "v1_10_R1"),
 	V1_11_R1("MC 1.11", "v1_11_R1"),
 	V1_12_R1("MC 1.12", "v1_12_R1"),
-	V1_13_R1("MC 1.13", "v1_13_R1"),
+	V1_13_R1("MC 1.13 Release 1", "v1_13_R1"),
+	V1_13_R2("MC 1.13 Release 2", "v1_13_R2"),
 	/**
-	 * Represents any unknown version of Spigot. This may include any version older than 1.8R1 or newer than 1.13R1
+	 * Represents any unknown version of Spigot. This may include any version older than 1.8R1 or newer than 1.13R2
 	 */
 	UNKNOWN("Unknown", "Unknown version");
 	
@@ -67,7 +68,6 @@ public enum NMSVersion {
 	
 	/**
 	 * Returns an array of all the 1.8 versions.<br>
-	 * This is equivalent to {@code new NMSVersion[] {V1_8_R1, V1_8_R2, V1_8_R3};}
 	 * @return An array containing all the 1.8 versions
 	 */
 	public static NMSVersion[] v1_8() {
@@ -76,8 +76,7 @@ public enum NMSVersion {
 
 	/**
 	 * Returns an array of all the 1.9 versions.<br>
-	 * This is equivalent to {@code new NMSVersion[] {V1_9_R1, V1_9_R2};}
-	 * @return An array containing all the 1.8 versions
+	 * @return An array containing all the 1.9 versions
 	 */
 	public static NMSVersion[] v1_9() {
 		return new NMSVersion[] {V1_9_R1, V1_9_R2};
@@ -85,20 +84,26 @@ public enum NMSVersion {
 
 	/**
 	 * Returns an array of all the versions from 1.8 to 1.12.<br>
-	 * This is equivalent to {@code new NMSVersion[] {V1_8_R1, V1_8_R2, V1_8_R3, V1_9_R1, V1_9_R2, V1_10_R1, V1_11_R1, V1_12_R1};}
-	 * @return An array containing all the 1.8 versions
+	 * @return An array containing all the 1.8-1.12 versions
 	 */
 	public static NMSVersion[] v1_8to1_12() {
-		return new NMSVersion[] {V1_8_R1, V1_8_R2, V1_8_R3, V1_9_R1, V1_9_R2, V1_10_R1, V1_11_R1, V1_12_R1};
+		return range(NMSVersion.V1_8_R1, NMSVersion.V1_12_R1);
 	}
 
 	/**
 	 * Returns an array of all the versions from 1.8 to 1.13.<br>
-	 * This is equivalent to {@code new NMSVersion[] {V1_8_R1, V1_8_R2, V1_8_R3, V1_9_R1, V1_9_R2, V1_10_R1, V1_11_R1, V1_12_R1, V1_13_R1};}
-	 * @return An array containing all the 1.8 versions
+	 * @return An array containing all the 1.8-1.13 versions
 	 */
 	public static NMSVersion[] v1_8to1_13() {
-		return new NMSVersion[] {V1_8_R1, V1_8_R2, V1_8_R3, V1_9_R1, V1_9_R2, V1_10_R1, V1_11_R1, V1_12_R1, V1_13_R1};
+		return range(NMSVersion.V1_8_R1, NMSVersion.V1_13_R2);
+	}
+
+	/**
+	 * Returns an array of all the 1.13 versions.<br>
+	 * @return An array containing all the 1.13 versions
+	 */
+	public static NMSVersion[] v1_13() {
+		return new NMSVersion[] {V1_13_R1, V1_13_R2};
 	}
 	
 }
