b3892a3d1c7348396b8a15de2a2f5bd0ca7a6c4e
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-10-24 16:52:27 +0200

    Updated CCv2

--- src/me/mrletsplay/mrcore/config/impl/ConfigLoader.java
+++ src/me/mrletsplay/mrcore/config/impl/ConfigLoader.java
@@ -3,8 +3,10 @@ package me.mrletsplay.mrcore.config.impl;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import me.mrletsplay.mrcore.config.v2.ConfigException;
+import me.mrletsplay.mrcore.config.v2.CustomConfig;
 import me.mrletsplay.mrcore.config.v2.FileCustomConfig;
 import me.mrletsplay.mrcore.config.v2.IncompatibleConfigVersionException;
 import me.mrletsplay.mrcore.io.IOUtils;
@@ -18,6 +20,20 @@ public class ConfigLoader {
 		}catch(IncompatibleConfigVersionException e) {
 			try {
 				return ConfigConverter.convertConfig(IOUtils.readAllBytes(new FileInputStream(configFile)), cc, e.getGivenDefaultVersion(), e.getRequiredDefaultVersion());
+			} catch (IOException e1) {
+				throw new ConfigException(e1);
+			}
+		}
+		return cc;
+	}
+
+	public static CustomConfig loadStreamConfig(InputStream stream) {
+		FileCustomConfigImpl cc = new FileCustomConfigImpl(null);
+		try {
+			cc.load(stream);
+		}catch(IncompatibleConfigVersionException e) {
+			try {
+				return ConfigConverter.convertConfig(IOUtils.readAllBytes(stream), cc, e.getGivenDefaultVersion(), e.getRequiredDefaultVersion());
 			} catch (IOException e1) {
 				throw new ConfigException(e1);
 			}
