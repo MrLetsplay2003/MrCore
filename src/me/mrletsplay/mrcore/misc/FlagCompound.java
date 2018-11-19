6c79871ffd4f2cbd2b7c1c0f4f8297671a9baf28
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2018-11-19 18:25:05 +0100

    Added documentation to FlagCompound

--- src/me/mrletsplay/mrcore/misc/FlagCompound.java
+++ src/me/mrletsplay/mrcore/misc/FlagCompound.java
@@ -8,63 +8,152 @@ public class FlagCompound {
 	
 	private long compound;
 	
+	/**
+	 * Creates a flag compound including all the given compounds (combined using OR)
+	 * @param compound The compounds to be included in this compound
+	 */
 	public FlagCompound(long... compound) {
 		for(long l : compound) {
 			this.compound |= l;
 		}
 	}
 	
-	public FlagCompound(Flag... compound) {
-		for(Flag l : compound) {
+	/**
+	 * Creates a flag compound including all the given flags
+	 * @param flags The flags to be included in this compound
+	 */
+	public FlagCompound(Flag... flags) {
+		for(Flag l : flags) {
 			this.compound |= l.getValue();
 		}
 	}
-	
+
+	/**
+	 * Creates a flag compound including all the given flags (combined using OR)
+	 * @param flags The flags to be included in this compound
+	 */
 	public FlagCompound(FlagCompound... compound) {
 		for(FlagCompound l : compound) {
 			this.compound |= l.compound;
 		}
 	}
 	
+	/**
+	 * Creates a flag compound from the given compound
+	 * @param compound The compound to include in this compound
+	 */
 	public FlagCompound(long compound) {
 		this.compound = compound;
 	}
 	
+	/**
+	 * Adds a flag (represented by a long) to this compound
+	 * @param flag The flag to add
+	 * @return This flag compound
+	 */
 	public FlagCompound addFlag(long flag) {
 		this.compound |= flag;
 		return this;
 	}
-	
+
+	/**
+	 * Adds a flag to this compound
+	 * @param flag The flag to add
+	 * @return This flag compound
+	 */
 	public FlagCompound addFlag(Flag flag) {
 		this.compound |= flag.getValue();
 		return this;
 	}
-	
+
+	/**
+	 * Adds all flags of another flag compound to this compound
+	 * @param flag The flag to add
+	 * @return This flag compound
+	 */
 	public FlagCompound addFlags(FlagCompound flag) {
 		this.compound |= flag.compound;
 		return this;
 	}
 	
+	/**
+	 * Removes a flag (represented by a long) from this compound
+	 * @param flag The flag to remove
+	 * @return This flag compound
+	 */
+	public FlagCompound removeFlag(long flag) {
+		this.compound ^= (this.compound & flag);
+		return this;
+	}
+
+	/**
+	 * Removes a flag from this compound
+	 * @param flag The flag to remove
+	 * @return This flag compound
+	 */
+	public FlagCompound removeFlag(Flag flag) {
+		this.compound ^= (this.compound & flag.getValue());
+		return this;
+	}
+
+	/**
+	 * Removes all flags of another flag compound from this compound
+	 * @param flag The flag to remove
+	 * @return This flag compound
+	 */
+	public FlagCompound removeFlags(FlagCompound flag) {
+		this.compound ^= (this.compound | flag.compound);
+		return this;
+	}
+	
+	/**
+	 * Checks whether a flag (represented by a long) is set in this flag compound
+	 * @param flag The flag to check
+	 * @return Whether that flag is set
+	 */
 	public boolean hasFlag(long flag) {
 		return (compound & flag) == flag;
 	}
-	
+
+	/**
+	 * Checks whether a flag is set in this flag compound
+	 * @param flag The flag to check
+	 * @return Whether that flag is set
+	 */
 	public boolean hasFlag(Flag flag) {
 		return (compound & flag.getValue()) == flag.getValue();
 	}
 	
-	public List<Flag> getApplicableFlags(List<Flag> flags) {
-		return flags.stream().filter(f -> hasFlag(f)).collect(Collectors.toList());
+	/**
+	 * Returns all flags from a given list that are set in this compound
+	 * @param flags The flags to check against
+	 * @return A sub list of all the flags that are set in this compound
+	 */
+	public List<? extends Flag> getApplicableFlags(List<? extends Flag> flags) {
+		return flags.stream().filter(this::hasFlag).collect(Collectors.toList());
 	}
-	
+
+	/**
+	 * Returns all flags from a given array that are set in this compound
+	 * @param flags The flags to check against
+	 * @return A sub list of all the flags that are set in this compound
+	 */
 	public List<Flag> getApplicableFlags(Flag... flags) {
-		return Arrays.stream(flags).filter(f -> hasFlag(f)).collect(Collectors.toList());
+		return Arrays.stream(flags).filter(this::hasFlag).collect(Collectors.toList());
 	}
 	
+	/**
+	 * Sets the compound for this flag compound
+	 * @param compound The compound to set to
+	 */
 	public void setCompound(long compound) {
 		this.compound = compound;
 	}
 	
+	/**
+	 * Returns the compound for this flag compound
+	 * @return The compound for this flag compound
+	 */
 	public long getCompound() {
 		return compound;
 	}
@@ -85,6 +174,13 @@ public class FlagCompound {
 		return new FlagCompound(flag1.compound | flag2.compound);
 	}
 	
+	/**
+	 * Combines two flags with the given combination mode
+	 * @param f1 A flag
+	 * @param f2 Another flag
+	 * @param mode The combination mode
+	 * @return The flags combined according to the given combination code
+	 */
 	public static FlagCompound combine(FlagCompound f1, FlagCompound f2, FlagCombinationMode mode) {
 		return new FlagCompound(mode.combineBulk(f1.compound, f2.compound));
 	}
@@ -97,10 +193,16 @@ public class FlagCompound {
 		
 	}
 	
+	/**
+	 * Defines default {@link FlagCombinationMode}s
+	 * @author MrLetsplay2003
+	 */
 	public static enum CombinationMode implements FlagCombinationMode {
 		
 		AND((BulkFlagCombinationMode) (a, b) -> a & b),
-		OR((BulkFlagCombinationMode) (a, b) -> a | b);
+		OR((BulkFlagCombinationMode) (a, b) -> a | b),
+		XOR((BulkFlagCombinationMode) (a, b) -> a ^ b),
+		;
 		
 		private FlagCombinationMode mode;
 		
