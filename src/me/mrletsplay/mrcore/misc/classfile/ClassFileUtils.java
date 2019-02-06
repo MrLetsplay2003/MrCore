d5512f020a582f5aeb08e33915eb84f51de4df6f
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2019-02-06 19:38:50 +0100

    Update ClassFile

--- src/me/mrletsplay/mrcore/misc/classfile/ClassFileUtils.java
+++ src/me/mrletsplay/mrcore/misc/classfile/ClassFileUtils.java
@@ -6,6 +6,7 @@ import java.util.List;
 import me.mrletsplay.mrcore.misc.PrimitiveType;
 import me.mrletsplay.mrcore.misc.classfile.attribute.AttributeCode;
 import me.mrletsplay.mrcore.misc.classfile.attribute.DefaultAttributeType;
+import me.mrletsplay.mrcore.misc.classfile.pool.entry.ConstantPoolClassEntry;
 import me.mrletsplay.mrcore.misc.classfile.pool.entry.ConstantPoolMethodRefEntry;
 import me.mrletsplay.mrcore.misc.classfile.pool.entry.ConstantPoolNameAndTypeEntry;
 import me.mrletsplay.mrcore.misc.classfile.pool.entry.ConstantPoolUTF8Entry;
@@ -13,9 +14,6 @@ import me.mrletsplay.mrcore.misc.classfile.pool.entry.ConstantPoolUTF8Entry;
 public class ClassFileUtils {
 	
 	public static void redirectMethodExecution(ClassFile file, ClassMethod method, String toClass, String toMethodName, String toMethodSig) {
-		System.out.println(
-				"REDIRECT " + file.getThisClass().getName().getValue().replace('/', '.') + "#" + method.getName().getValue() + ":" + method.getDescriptor().getValue() +
-				" to " + toClass + "#" + toMethodName + ":" + toMethodSig);
 		AttributeCode codeAttr = (AttributeCode) method.getAttribute(DefaultAttributeType.CODE);
 		List<InstructionInformation> nInstr = new ArrayList<>();
 		
@@ -25,20 +23,27 @@ public class ClassFileUtils {
 		int dInd = file.getConstantPool().appendEntry(dEn);
 		ConstantPoolNameAndTypeEntry ntEn = new ConstantPoolNameAndTypeEntry(file.getConstantPool(), nInd, dInd);
 		int ntInd = file.getConstantPool().appendEntry(ntEn);
-		ConstantPoolMethodRefEntry mrEn = new ConstantPoolMethodRefEntry(file.getConstantPool(), file.getConstantPool().indexOf(file.getThisClass()), ntInd);
+		ConstantPoolUTF8Entry clNEn = new ConstantPoolUTF8Entry(file.getConstantPool(), toClass);
+		int clNInd = file.getConstantPool().appendEntry(clNEn);
+		ConstantPoolClassEntry clEn = new ConstantPoolClassEntry(file.getConstantPool(), clNInd);
+		int clInd = file.getConstantPool().appendEntry(clEn);
+		ConstantPoolMethodRefEntry mrEn = new ConstantPoolMethodRefEntry(file.getConstantPool(), clInd, ntInd);
 		int mrInd = file.getConstantPool().appendEntry(mrEn);
 		
-		nInstr.add(new InstructionInformation(Instruction.ALOAD_0, new byte[] {}));
+		boolean isStatic = method.getAccessFlags().hasFlag(MethodAccessFlag.STATIC);
+		if(!isStatic) { // Only add this class instance for instance methods
+			nInstr.add(new InstructionInformation(Instruction.ALOAD_0, new byte[] {}));
+		}
 		byte i = 0;
 		for(ParameterDescriptor p : method.getMethodDescriptor().getParameterDescriptors()) {
 			nInstr.add(getInfo(p.getParameterType(), ++i));
 			PrimitiveType t = p.getParameterType().getPrimitiveType();
 			if(t.equals(PrimitiveType.DOUBLE) || t.equals(PrimitiveType.LONG)) i++;
 		}
-		nInstr.add(new InstructionInformation(Instruction.INVOKEVIRTUAL, getShortBytes(mrInd)));
+		nInstr.add(new InstructionInformation(Instruction.INVOKESTATIC, getShortBytes(mrInd)));
 		nInstr.add(new InstructionInformation(Instruction.RETURN, new byte[] {}));
 		
-		codeAttr.setMaxStack(i + 1); // #params + 1 for "this"
+		codeAttr.setMaxStack(i + (isStatic ? 0 : 1)); // #params + 1 for this class instance if instance method
 		
 		codeAttr.getCode().replace(ByteCode.of(nInstr));
 		System.out.println(nInstr);
