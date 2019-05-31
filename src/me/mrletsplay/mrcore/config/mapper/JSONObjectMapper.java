6a2d1824f9ccb5bd59e3f7069086cb17426508c3
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2019-05-31 17:38:20 +0200

    Update JSONObjectMapper

--- src/me/mrletsplay/mrcore/config/mapper/JSONObjectMapper.java
+++ src/me/mrletsplay/mrcore/config/mapper/JSONObjectMapper.java
@@ -5,6 +5,8 @@ import java.util.function.BiFunction;
 import me.mrletsplay.mrcore.config.ConfigSection;
 import me.mrletsplay.mrcore.config.impl.JSONObjectMapperImpl;
 import me.mrletsplay.mrcore.json.JSONObject;
+import me.mrletsplay.mrcore.json.converter.JSONConverter;
+import me.mrletsplay.mrcore.json.converter.JSONConvertible;
 import me.mrletsplay.mrcore.misc.Complex;
 
 public interface JSONObjectMapper<E> extends ObjectMapper<E, JSONObject> {
@@ -20,6 +22,10 @@ public interface JSONObjectMapper<E> extends ObjectMapper<E, JSONObject> {
 	
 	public static <E> JSONObjectMapper<E> create(Complex<E> mappingClass, BiFunction<ConfigSection, E, JSONObject> mappingFunction, BiFunction<ConfigSection, JSONObject, E> constructingFunction) {
 		return new JSONObjectMapperImpl<>(mappingClass, mappingFunction, constructingFunction);
+	}
+	
+	public static <E extends JSONConvertible> JSONObjectMapper<E> create(Class<E> mappingClass) {
+		return new JSONObjectMapperImpl<>(Complex.value(mappingClass), (s, e) -> e.toJSON(), (s, j) -> JSONConverter.decodeObject(j, mappingClass));
 	}
 	
 }
