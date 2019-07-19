27ba3c23a367bd8895c2ccd275ce10c264dbbfa4
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2019-07-19 23:26:46 +0200

    Update version

--- src/me/mrletsplay/mrcore/main/MrCore.java
+++ src/me/mrletsplay/mrcore/main/MrCore.java
@@ -2,7 +2,7 @@
 
 public class MrCore {
 	
-	private static final String VERSION = "2.0b10";
+	private static final String VERSION = "2.0b11";
 	
 	public static String getVersion() {
 		return VERSION;
