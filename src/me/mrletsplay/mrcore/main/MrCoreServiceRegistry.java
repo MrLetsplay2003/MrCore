b8542b3628a40db9cefbdfeccf71e13fdf6312dc
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2019-08-11 13:28:18 +0200

    Update MrCoreServiceRegistry

--- src/me/mrletsplay/mrcore/main/MrCoreServiceRegistry.java
+++ src/me/mrletsplay/mrcore/main/MrCoreServiceRegistry.java
@@ -9,20 +9,47 @@ import java.util.concurrent.CompletableFuture;
 public class MrCoreServiceRegistry {
 	
 	private static Map<String, Object> registeredServices = new HashMap<>();
-	private static Map<String, List<CompletableFuture<Object>>> waitingRegistrations;
+	private static Map<String, List<CompletableFuture<Object>>> waitingRegistrations = new HashMap<>();
 	
 	public static void registerService(String name, Object service) {
-		registeredServices.put(name, service);
-		waitingRegistrations.getOrDefault(name, new ArrayList<>()).forEach(f -> f.complete(service));
+		registeredServices.put(name.toLowerCase(), service);
+		waitingRegistrations.getOrDefault(name.toLowerCase(), new ArrayList<>()).forEach(f -> f.complete(service));
+	}
+	
+	public static boolean isServiceRegistered(String name) {
+		return registeredServices.containsKey(name.toLowerCase());
+	}
+	
+	public static Object getRegisteredService(String name) {
+		return registeredServices.get(name.toLowerCase());
+	}
+	
+	public static <T> T getRegisteredService(String name, Class<T> type) {
+		return type.cast(getRegisteredService(name));
+	}
+	
+	public static CompletableFuture<Object> awaitServiceRegistration(String name) {
+		if(isServiceRegistered(name)) {
+			CompletableFuture<Object> f = new CompletableFuture<>();
+			f.complete(getRegisteredService(name));
+			return f;
+		}
+		CompletableFuture<Object> f = new CompletableFuture<>();
+		List<CompletableFuture<Object>> l = waitingRegistrations.getOrDefault(name.toLowerCase(), new ArrayList<>());
+		l.add(f);
+		waitingRegistrations.put(name.toLowerCase(), l);
+		return f;
 	}
 	
 	public static <T> CompletableFuture<T> awaitServiceRegistration(String name, Class<T> type) {
+		if(isServiceRegistered(name)) {
+			CompletableFuture<T> f = new CompletableFuture<>();
+			f.complete(getRegisteredService(name, type));
+			return f;
+		}
 		CompletableFuture<T> f = new CompletableFuture<>();
-		CompletableFuture<Object> f2 = new CompletableFuture<>();
+		CompletableFuture<Object> f2 = awaitServiceRegistration(name);
 		f2.thenAccept(obj -> f.complete(type.cast(obj)));
-		List<CompletableFuture<Object>> l = waitingRegistrations.getOrDefault(name, new ArrayList<>());
-		l.add(f2);
-		waitingRegistrations.put(name, l);
 		return f;
 	}
 	
