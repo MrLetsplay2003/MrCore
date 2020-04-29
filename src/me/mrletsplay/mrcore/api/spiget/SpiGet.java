d34a191f4e10828956aca9920cbc47fc906403c0
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2020-04-30 00:04:32 +0200

    Fix SpiGet API

--- src/me/mrletsplay/mrcore/api/spiget/SpiGet.java
+++ src/me/mrletsplay/mrcore/api/spiget/SpiGet.java
@@ -1,7 +1,9 @@
 package me.mrletsplay.mrcore.api.spiget;
 
+import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,9 +17,15 @@ public class SpiGet {
 	public static final String API_BASE_URL = "https://api.spiget.org/v2/";
 	
 	public static List<SpiGetResource.Search> searchResource(String query) {
-		return new JSONArray(makeRequest(API_BASE_URL + "search/resources/" + urlEncode(query) + "?size=1000&field=name")).stream()
-			.map(res -> new SpiGetResource.Search((JSONObject) res))
-			.collect(Collectors.toList());
+		try {
+			return new JSONArray(makeRequest(API_BASE_URL + "search/resources/" + urlEncode(query) + "?size=1000&field=name")).stream()
+				.map(res -> new SpiGetResource.Search((JSONObject) res))
+				.collect(Collectors.toList());
+		}catch(IllegalStateException e) {
+			if(e.getCause() instanceof FileNotFoundException)
+				return Collections.emptyList();
+			throw e;
+		}
 	}
 	
 	public static SpiGetResource getResource(int resourceID) {
