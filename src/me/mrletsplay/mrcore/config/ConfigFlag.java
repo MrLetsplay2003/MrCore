385f241d07f507e7e3728f04f888533d67a8eb40
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2020-06-06 16:05:13 +0200

    Update ConfigFlag javadoc

--- src/me/mrletsplay/mrcore/config/ConfigFlag.java
+++ src/me/mrletsplay/mrcore/config/ConfigFlag.java
@@ -4,13 +4,14 @@ public enum ConfigFlag {
 
 	/**
 	 * When saving the config, all config properties will be sorted alphabetically<br>
+	 * @deprecated Currently does nothing
 	 */
 	@Deprecated
 	SORT_ALPHABETICALLY,
 	
 	/**
 	 * When saving the config, properties with comments will have one empty line added before and after them<br>
-	 * Currently does nothing
+	 * @deprecated Currently does nothing
 	 */
 	@Deprecated
 	SPACE_COMMENTED_PROPERTIES,
