fadfc84b0448a9e486e8fe9081cca6c38ebaa3a6
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2020-12-19 13:59:02 +0100

    Fix version

--- src/me/mrletsplay/mrcore/main/MrCore.java
+++ src/me/mrletsplay/mrcore/main/MrCore.java
@@ -2,7 +2,7 @@
 
 public class MrCore {
 	
-	private static final String VERSION = "2.8.3";
+	private static final String VERSION = "2.8.4";
 	
 	public static String getVersion() {
 		return VERSION;
