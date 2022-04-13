e14a357c845c11c97fdd00757a5faf5c4a413fab
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2022-04-13 19:16:24 +0200

    Allow serialization options for JSONObjectMapper

--- src/me/mrletsplay/mrcore/config/mapper/JSONObjectMapper.java
+++ src/me/mrletsplay/mrcore/config/mapper/JSONObjectMapper.java
@@ -1,12 +1,15 @@
 package me.mrletsplay.mrcore.config.mapper;
 
+import java.util.EnumSet;
 import java.util.function.BiFunction;
 
 import me.mrletsplay.mrcore.config.ConfigSection;
 import me.mrletsplay.mrcore.config.impl.JSONObjectMapperImpl;
 import me.mrletsplay.mrcore.json.JSONObject;
+import me.mrletsplay.mrcore.json.converter.DeserializationOption;
 import me.mrletsplay.mrcore.json.converter.JSONConverter;
 import me.mrletsplay.mrcore.json.converter.JSONConvertible;
+import me.mrletsplay.mrcore.json.converter.SerializationOption;
 import me.mrletsplay.mrcore.misc.Complex;
 
 public interface JSONObjectMapper<E> extends ObjectMapper<E, JSONObject> {
@@ -26,6 +29,10 @@ public interface JSONObjectMapper<E> extends ObjectMapper<E, JSONObject> {
 	
 	public static <E extends JSONConvertible> JSONObjectMapper<E> create(Class<E> mappingClass) {
 		return new JSONObjectMapperImpl<>(Complex.value(mappingClass), (s, e) -> e.toJSON(), (s, j) -> JSONConverter.decodeObject(j, mappingClass));
+	}
+	
+	public static <E extends JSONConvertible> JSONObjectMapper<E> create(Class<E> mappingClass, EnumSet<SerializationOption> serializationOptions, EnumSet<DeserializationOption> deserializationOptions) {
+		return new JSONObjectMapperImpl<>(Complex.value(mappingClass), (s, e) -> e.toJSON(serializationOptions), (s, j) -> JSONConverter.decodeObject(j, mappingClass, deserializationOptions));
 	}
 	
 }
